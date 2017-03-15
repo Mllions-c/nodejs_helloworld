5f998977a8fc22fb5a4ff0fb4a1ac7f077efa672
Millions <millions_02@sina.com>
2017-03-15 08:51:06 +0800

    node_15获取文件夹信息

---
+++ node_15获取文件夹信息.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dingyl on 2017/3/10.
+ */
+//fs操作
+var fs = require('fs');
+
+//获取文件或目录的详情\
+
+
+//fs.readdir('dir1',function(err,files){
+//
+//
+//    files.forEach(function(file){
+//
+//        fs.stat('dir1/'+file,function(err,info){
+//            console.log(info)
+//            switch (info.mode){
+//                case 33206:
+//                    console.log(file+'：[文件]/长度是'+info.size)
+//                    break;
+//                case 16822:
+//                    console.log(file+'：[文件夹]/长度是'+info.size)
+//            }
+//        })
+//
+//
+//    })
+//
+//})
+
+
